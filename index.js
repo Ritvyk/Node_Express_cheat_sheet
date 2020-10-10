51923581e1c07e7e6998e3325af149095b514b7c
Ritvik <ritvyk@gmail.com>
2020-10-10 17:40:47 +0530

    middlewares added

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const path = require("path");
 const express = require("express");
 const membersAPI = require("./Members");
+const { query } = require("express");
+
+const app = express(); //this will handle all the requests
+
+const logger = (req, res, next) => {
+  console.log("First Middleware -- Logger");
+  next(); //calling the next middleware that is under /member/login controller --> auth
+};
+
+const auth = (req, res, next) => {
+  if (req.query.admin === "true") {
+    next(); //calling the next middleware that is after auth in /member/login controller
+  } else {
+    res.send("Not Authorized");
+  }
+};
+
+app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//Routing
+app.use("/api/members", require(path.join(__dirname, "public/api/fakeAPI.js")));
+
+//member/login controller
+app.get("/member/login", auth, (req, res) => {
+  res.send("User Page");
+});
 
 //setting to enviroment variables
 require("dotenv").config();
@@ -8,13 +35,6 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 //make fake data
 membersAPI.makeMembers(10);
-
-const app = express(); //this will handle all the requests
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-//Routing
-app.use("/api/members", require(path.join(__dirname, "public/api/fakeAPI.js")));
 
 app.listen(PORT, () => {
   console.log(`Server started on Port : ${PORT}`);
